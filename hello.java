36d1a70ee5c0aec6caf316956720427fc94d4e61
Anastasia <alaity47@users.noreply.github.com>
2017-04-05 09:36:24 -0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
    public static void main(String args[])
    {
-      System.out.println("Hello Workshop I have changed");
+      System.out.println("Hello Workshop I have changed I HAVE CHANGED AGAIN!!!");
    }
 }
