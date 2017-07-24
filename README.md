2447d2030926a79f934f2c3c028bf15487ce17f3
maswain <mswain@ipac.caltech.edu>
2017-07-24 12:32:20 -0700

    Add check_versions.sh

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # stil-BTE-demo
-Software to be run in BTE demo
+***Software to be run in BTE demo***
+
+- HelloWorld.class
+- hello.java
+- hello.scala
+- hello_with_param.scala
+- schurr.test.file
+- check_versions.sh
---
+++ check_versions.sh
@@ -0,0 +1,129 @@
+#!/bin/bash +x
+echo "*****************************"
+echo " Java"
+echo " java -version"
+echo "*****************************"
+java -version
+echo ""
+
+
+echo "*****************************"
+echo " Scala"
+echo " scala -version"
+echo "*****************************"
+scala -version
+echo ""
+
+
+echo "*****************************"
+echo " GCC"
+echo " gcc --version"
+echo "*****************************"
+gcc --version
+echo ""
+
+
+echo "*****************************"
+echo " CMAKE"
+echo " cmake -version"
+echo "*****************************"
+cmake -version
+echo ""
+
+
+echo "*****************************"
+echo " LLVM"
+echo " /usr/bin/llvm-link --version | grep \"LLVM version\""
+echo "*****************************"
+/usr/bin/llvm-link --version | grep "LLVM version"
+echo ""
+
+
+echo "*****************************"
+echo " CLANG"
+echo " clang --version"
+echo "*****************************"
+clang --version
+echo ""
+
+
+echo "*****************************"
+echo " CHECKSTYLE"
+echo " ls -atlar /usr/local/checkstyle/*.jar"
+echo "*****************************"
+ls -atlar /usr/local/checkstyle/*.jar
+echo ""
+
+
+echo "*****************************"
+echo " CPPCHECK"
+echo " cppcheck --version"
+echo "*****************************"
+cppcheck --version
+echo ""
+
+
+echo "*****************************"
+echo " SCALAFMT"
+echo " /usr/local/scalafmt/scalafmt --version"
+echo "*****************************"
+/usr/local/scalafmt/scalafmt --version
+echo ""
+
+
+echo "*****************************"
+echo " SCALASTYLE"
+echo " ls -d /usr/local/scalastlye/*.jar"
+echo "*****************************"
+ls -d /usr/local/scalastlye/*.jar
+echo ""
+
+
+echo "*****************************"
+echo " MAVEN"
+echo " /usr/share/maven/bin/mvn --version"
+echo "*****************************"
+/usr/share/maven/bin/mvn --version
+echo ""
+
+
+echo "*****************************"
+echo " PMD"
+echo " ls -d /usr/local/pmd/pmd-bin*"
+echo "*****************************"
+ls -d /usr/local/pmd/pmd-bin*
+echo ""
+
+
+echo "*****************************"
+echo " SONARQUBE"
+echo " ls -d /usr/local/sonarqube/* | grep -v zip"
+echo " /usr/local/sonarqube-scanner/*-linux/bin/sonar-scanner --version"
+echo "*****************************"
+ls -d /usr/local/sonarqube/* | grep -v zip
+/usr/local/sonarqube-scanner/*-linux/bin/sonar-scanner --version
+echo ""
+
+
+echo "*****************************"
+echo " SBT"
+echo " sbt sbt-version"
+echo "*****************************"
+sbt sbt-version
+echo ""
+
+
+echo "*****************************"
+echo " JENKINS"
+echo " java -jar /usr/lib/jenkins/jenkins.war --version"
+echo "*****************************"
+java -jar /usr/lib/jenkins/jenkins.war --version                                                                                                      
+echo "" 
+
+
+echo "*****************************"
+echo " ENV"
+echo " env"
+echo "*****************************"
+env
+echo ""
